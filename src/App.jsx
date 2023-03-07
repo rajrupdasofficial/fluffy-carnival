fb57cfc294ba10c66545848e64e9278ad62166e2
RajrupDasid <rajrupdasid@keemail.me>
2023-03-07 09:49:11 +0530

    initial commit with tailwindcss added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-red-500'>Testapp</div>
+  )
+}
+
+export default App
