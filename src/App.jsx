c88bc9e7c1bf4974440be0ed8b1ab849ece58b72
RajrupDasid <rajrupdasid@keemail.me>
2023-03-07 12:48:30 +0530

    axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import Register from "./register";
 
 function App() {
+  axios.defaults.baseURL='http://localhost:3999';
+  axios.defaults.withCredentials=true;
   return (
     <Register/>
   )
