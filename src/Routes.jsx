3f5a6e305b007c32460634714489c5b67d9b090c
RajrupDasid <rajrupdasid@keemail.me>
2023-03-08 00:56:59 +0530

    chat field added

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { UserContext } from "./UserContext";
+import Chat from "./chat";
 
 
 export default function Routes(){
     const {username,id} = useContext(UserContext);
     if (username){
-        return "loggedin"+" "+username;
+        return <Chat/>
     }
     return(
         <RegisterAndLoginForm/>
